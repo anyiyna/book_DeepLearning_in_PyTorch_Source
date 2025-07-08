0d34aed58946109170163e14256f509338b37897
anyiyna <althmann@163.com>
2025-07-08 18:03:25 +0800

    Update a.py

--- 联系/a.py
+++ 联系/a.py
@@ -4,6 +4,12 @@ import torch
 import torch.optim as optim
 import matplotlib.pyplot as plt
 
-data_path = "book_DeepLearning_in_PyTorch_Source/03_bike_predictor/bike-sharing-dataset/hour.csv"
+data_path = '/Users/anyi/Documents/GitHub/book_DeepLearning_in_PyTorch_Source/03_bike_predictor/bike-sharing-dataset/hour.csv'
 rides = pd.read_csv(data_path)
-count = rides['count'].values
+counts = rides['cnt'][:50]
+x = np.arange(len(counts))
+y = np.array(counts)
+plt.figure(figsize=(10, 5))
+plt.plot(x,y,'0-')
+plt.xlabel('X')
+plt.ylabel('Y')
