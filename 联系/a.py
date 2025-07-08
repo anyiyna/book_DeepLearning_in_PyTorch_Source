78583750bb4e1d7ef7ede61291cac26051d496a1
anyiyna <althmann@163.com>
2025-07-08 17:27:49 +0800

    Update a.py

--- 联系/a.py
+++ 联系/a.py
@@ -3,3 +3,7 @@ import pandas as pd
 import torch
 import torch.optim as optim
 import matplotlib.pyplot as plt
+
+data_path = "book_DeepLearning_in_PyTorch_Source/03_bike_predictor/bike-sharing-dataset/hour.csv"
+rides = pd.read_csv(data_path)
+count = rides['count'].values
